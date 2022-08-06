a6d67bf8abc853f54c97e97e5f2d3480b164d031
SpaceChae <enaenen@naver.com>
2022-08-06 23:26:57 +0900

    ex02

---
+++ ex01/Zombie.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <iostream>
+
+#define ZOMBIE_MESSAGE "BraiiiiiiinnnzzzZ..."
+
+class Zombie
+{
+    private:
+        std::string name;
+    public:
+        Zombie(std::string name);
+        ~Zombie();
+        void    announce(void);
+};
+
+Zombie* newZombie(std::string name);
+Zombie* zombieHorde(int n, std::string name);
---
+++ ex01/ZombieHorde.cpp
@@ -0,0 +1,6 @@
+#include "Zombie.hpp"
+
+Zombie* zombieHorde(int n, std::string name)
+{
+    
+}
