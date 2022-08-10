503cae36f97c14bcdd54712b259068f8b50d62eb
SpaceChae <enaenen@naver.com>
2022-08-10 20:05:22 +0900

    std delete

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -11,17 +11,17 @@ int main(int argc, char **argv)
     
     int n;
     if (2 < argc)
-        n = std::atoi(argv[2]);
+        n = atoi(argv[2]);
     else
         n = 1;
     if (n < 0)
     {
         std::cout << "n out of range (minimum 0)" << std::endl;
-        return EXIT_FAILURE;
+        return 1;
     }
     Zombie* horde = zombieHorde(n, zombieName);
     for (int i = 0; i < n; i++)
         horde[i].announce();
     delete[] horde;
-    return EXIT_SUCCESS;
+    return 0;
 }
