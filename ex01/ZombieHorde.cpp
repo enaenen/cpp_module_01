a15e90e5c62e579864d94c3d3a4baf0a2ef7baf6
SpaceChae <enaenen@naver.com>
2022-08-10 20:17:07 +0900

    to_string to stringstream

--- ex01/ZombieHorde.cpp
+++ ex01/ZombieHorde.cpp
@@ -1,9 +1,14 @@
 #include "Zombie.hpp"
+#include <sstream>
 
 Zombie* zombieHorde(int n, std::string name)
 {
     Zombie* zombies = new Zombie[n];
     for (int i = 0; i < n; i++)
-        zombies[i].setName(name + " " +(std::to_string(i)));
+    {
+        std::stringstream ss;
+        ss << i << " " << name;
+        zombies[i].setName(ss.str());
+    }
     return zombies;
 }
