403fa0052f7eb35fc8219be1bf701fc0913bfb45
SpaceChae <enaenen@naver.com>
2022-08-16 17:13:28 +0900

    complete

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -1,6 +1,18 @@
 #include <fstream>
 #include <iostream>
 
+std::string replace(const std::string &str, const std::string &from,
+                    const std::string &to) {
+  std::string text = str;
+  size_t pos = 0;
+  while ((pos = text.find(from, pos)) != std::string::npos) {
+    text.erase(pos, from.length());
+    text.insert(pos, to);
+    pos += to.length();
+  }
+  return (text);
+}
+
 int main(int argc, char **argv) {
   // Program Argument check
   if (argc < 4) {
@@ -9,6 +21,7 @@ int main(int argc, char **argv) {
   }
   std::string fileName(argv[1]);
   std::string fromString(argv[2]);
+  std::string toString(argv[3]);
   // Argument valid check
   if (fileName.empty()) {
     std::cout << "./ex04 <filename> <from> <to>" << std::endl;
@@ -31,10 +44,11 @@ int main(int argc, char **argv) {
   // output file settings
   std::ofstream outputFile;
   outputFile.open((fileName + ".replace").c_str());
-  outputFile << "xxxx" << std::endl;
+
+  // replace process
   std::string readString;
   while (getline(inputFile, readString)) {
-    outputFile << readString << std::endl;
+    outputFile << replace(readString, fromString, toString) << std::endl;
   }
   // stream closes
   inputFile.close();
