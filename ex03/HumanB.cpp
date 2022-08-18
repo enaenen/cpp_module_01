9d4722e418424ea1964da9b1c4461dcbdf1d9f2b
SpaceChae <enaenen@naver.com>
2022-08-18 22:20:44 +0900

    ex03

--- ex03/HumanB.cpp
+++ ex03/HumanB.cpp
@@ -8,5 +8,8 @@ void HumanB::attack() {
   if (this->weapon)
     std::cout << this->name << " attacks with their " << this->weapon->getType()
               << std::endl;
+  else
+    std::cout << this->name << " attacks with their "
+              << "Fist" << std::endl;
 };
 void HumanB::setWeapon(Weapon& weapon) { this->weapon = &weapon; }
--- ex03/main.cpp
+++ ex03/main.cpp
@@ -18,5 +18,15 @@ int main() {
     club.setType("some other type of club");
     jim.attack();
   }
+
+  {
+    Weapon club = Weapon("crude spiked club");
+    HumanB jim("Jim");
+    jim.attack();
+    jim.setWeapon(club);
+    jim.attack();
+    club.setType("some other type of club");
+    jim.attack();
+  }
   return (0);
 }
