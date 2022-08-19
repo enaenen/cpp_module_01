745d70a8d1bd2554289fbc0533d3d52daca4d122
SpaceChae <enaenen@naver.com>
2022-08-19 19:15:48 +0900

    testfix

--- ex05/main.cpp
+++ ex05/main.cpp
@@ -4,19 +4,29 @@ int main(void) {
   Harl harl;
 
   // NOTHING
+  std::cout << " harl.complain(\"xxxx\")" << std::endl;
   harl.complain("xxxx");
+  std::cout << std::endl;
 
   // DEBUG TEST
+  std::cout << " harl.complain(\"DEBUG\")" << std::endl;
   harl.complain("DEBUG");
+  std::cout << std::endl;
 
   // WARNING TEST
+  std::cout << " harl.complain(\"WARNING\")" << std::endl;
   harl.complain("WARNING");
+  std::cout << std::endl;
 
   // INFO TEST
+  std::cout << " harl.complain(\"INFO\")" << std::endl;
   harl.complain("INFO");
+  std::cout << std::endl;
 
   // ERROR TEST
+  std::cout << " harl.complain(\"ERROR\")" << std::endl;
   harl.complain("ERROR");
+  std::cout << std::endl;
 
   return (0);
 }
